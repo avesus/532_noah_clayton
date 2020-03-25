083fc37048ab9531ee74fa6b5f60c76690a586de
Noah Goldstein <goldstein.n@wustl.edu>
2020-03-25 00:05:01 -0500

    sutdio 13 done (under s12)

--- shared_stuff/studios/s12/ace_client/aclient.cpp
+++ shared_stuff/studios/s12/ace_client/aclient.cpp
@@ -4,6 +4,13 @@
 #include "ace/INET_Addr.h"
 #include "ace/SOCK_Stream.h"
 #include "ace/SOCK_Connector.h"
+#include "ace/Event_Handler.h"
+#include "ace/Timer_Queue_Adapters.h"
+#include "ace/OS.h"
+#include "ace/Reactor.h"
+#include "ace/Timer_Heap_T.h"
+#include "ace/Timer_Heap.h"
+#include "ace/Event_Handler_Handle_Timeout_Upcall.h"
 
 #define SUCCESS 0
 #define FAILURE (-1)
@@ -11,6 +18,49 @@
 #define PORT_NUM 6005
 
 #define MIN_ARGS 2
+
+
+class my_ev_handler: public ACE_Event_Handler {
+  int n_to_send;
+  char ** to_send;
+public:
+  my_ev_handler(int send_argc, char ** send_argv) {
+    this->n_to_send = send_argc;
+    this->to_send = send_argv;
+  }
+  
+  int handle_timeout (const ACE_Time_Value &current_time,
+		      const void *act) {
+
+    ACE_INET_Addr ace_addr(PORT_NUM, ACE_LOCALHOST);
+    ACE_SOCK_Stream ace_stream;
+    ACE_SOCK_Connector ace_connect;
+
+
+    if(ace_connect.connect(ace_stream, ace_addr) < 0){
+      fprintf(stderr, "Error connecting\n");
+      return FAILURE;
+    }
+
+    char send_buf[32];
+    for(int i=0; i<this->n_to_send; i++){
+      sprintf(send_buf, "%s\n", this->to_send[i]);
+      if(ace_stream.send_n(send_buf, strlen(send_buf)) < 0){
+	fprintf(stderr, "Error sending (%d)\n", i);
+	return FAILURE;
+      }
+    }
+    ace_stream.close();
+    return SUCCESS;
+  }
+};
+
+
+void errdie(){
+  fprintf(stderr, "Timeout occured\n");
+  exit(FAILURE);
+}
+
 int main(int argc, char * argv[]){
   if(argc < MIN_ARGS){
     fprintf(stderr, "Error no arguments detected\n");
@@ -18,25 +68,12 @@ int main(int argc, char * argv[]){
   }
 
 
-  ACE_INET_Addr ace_addr(PORT_NUM, ACE_LOCALHOST);
-  ACE_SOCK_Stream ace_stream;
-  ACE_SOCK_Connector ace_connect;
+  my_ev_handler sender(argc, argv);
 
 
-  if(ace_connect.connect(ace_stream, ace_addr) < 0){
-    fprintf(stderr, "Error connecting\n");
-    exit(FAILURE);
-  }
-
-  char send_buf[256] = "";
-    for(int i=0; i<argc; i++){
-      sprintf(send_buf, "%s\n", argv[i]);
-      if(ace_stream.send_n(send_buf, strlen(send_buf)) < 0){
-	fprintf(stderr, "Error sending (%d)\n", i);
-      }
-    }
-    ace_stream.close();
-
-
+  ACE_Time_Value t_interval(3, 0);
+  ACE_Time_Value t_delay(0, 0);
+  ACE_Reactor::instance()->schedule_timer(&sender, 0, t_delay, t_interval);
+  ACE_Reactor::instance()->run_reactor_event_loop();
   return SUCCESS;
 }
--- shared_stuff/studios/s12/ace_server/aserver.cpp
+++ shared_stuff/studios/s12/ace_server/aserver.cpp
@@ -4,6 +4,14 @@
 #include "ace/INET_Addr.h"
 #include "ace/SOCK_Stream.h"
 #include "ace/SOCK_Acceptor.h"
+#include "ace/Event_Handler.h"
+#include "ace/Timer_Queue_Adapters.h"
+#include "ace/OS.h"
+#include "ace/Reactor.h"
+#include "ace/Timer_Heap_T.h"
+#include "ace/Timer_Heap.h"
+#include "ace/Event_Handler_Handle_Timeout_Upcall.h"
+
 
 #define SUCCESS 0
 #define FAILURE (-1)
@@ -11,6 +19,49 @@
 #define PORT_NUM 6005
 
 #define NARGS 1
+
+
+
+class my_ev_handler: public ACE_Event_Handler {
+  ACE_INET_Addr ace_addr;
+  ACE_SOCK_Acceptor ace_accptr;
+public:
+  my_ev_handler(ACE_SOCK_Acceptor &accptr) : ace_accptr(accptr), ace_addr(PORT_NUM, ACE_LOCALHOST) {
+    if(ace_accptr.open(ace_addr, 1) < 0){
+      fprintf(stderr, "Error opening acceptor\n");
+    }
+  }
+
+  int handle_input (ACE_HANDLE h = ACE_INVALID_HANDLE) {
+
+    ACE_SOCK_Stream ace_stream;
+    
+    char addr_to_str_buf[256] = "";
+    ace_addr.addr_to_string(addr_to_str_buf, 256);
+    fprintf(stderr, "New Acceptor On ->\n\t%s\n", addr_to_str_buf);
+
+    if(ace_accptr.accept(ace_stream) < 0){
+      fprintf(stderr, "Error calling accept\n");
+      return FAILURE;
+    }
+    size_t ret = 0;
+    char single_read;
+    while((ret = ace_stream.recv_n(&single_read, 1)) > 0){
+      fprintf(stderr, "%c", single_read);
+    }
+    ace_stream.close();
+    return SUCCESS;
+  }
+
+
+
+  ACE_HANDLE get_handle() const {
+    return this->ace_accptr.get_handle();
+  }
+ 
+};
+
+
 int main(int argc, char * argv[]){
   if(argc != NARGS){
     fprintf(stderr, "Error invalid arguments passed\n");
@@ -21,26 +72,14 @@ int main(int argc, char * argv[]){
 	  "Arg[0]: %s\n",
 	  argv[0]);
 
-  ACE_INET_Addr ace_addr(PORT_NUM, ACE_LOCALHOST);
-  ACE_SOCK_Stream ace_stream;
-  ACE_SOCK_Acceptor ace_accptr;
-  if(ace_accptr.open(ace_addr, 1) < 0){
-    fprintf(stderr, "Error opening acceptor\n");
-    exit(FAILURE);
-  }
-  char addr_to_str_buf[256] = "";
-  ace_addr.addr_to_string(addr_to_str_buf, 256);
-  fprintf(stderr, "New Acceptor On ->\n\t%s\n", addr_to_str_buf);
+  ACE_SOCK_Acceptor accptr;
+  my_ev_handler sender(accptr);
+  ACE_Reactor::instance()->register_handler(&sender,
+					    ACE_Event_Handler::ACCEPT_MASK);
+  ACE_Reactor::instance()->run_reactor_event_loop();
+  //  while(sender.handle_input() == SUCCESS);
 
-  if(ace_accptr.accept(ace_stream) < 0){
-    fprintf(stderr, "Error calling accept\n");
-    exit(FAILURE);
-  }
-  size_t ret = 0;
-  char single_read;
-  while((ret = ace_stream.recv_n(&single_read, 1)) > 0){
-    fprintf(stderr, "%c", single_read);
-  }
+    
 
-  return SUCCESS;
+  return FAILURE;
 }

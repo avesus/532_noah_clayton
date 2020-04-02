56fbc9bc70c11c541298d98115323e802cfd345f
Noah Goldstein <goldstein.n@wustl.edu>
2020-04-01 21:00:32 -0500

    studio 12-15

--- shared_stuff/studios/s12/ace_client/aclient.cpp
+++ shared_stuff/studios/s12/ace_client/aclient.cpp
@@ -11,6 +11,9 @@
 #include "ace/Timer_Heap_T.h"
 #include "ace/Timer_Heap.h"
 #include "ace/Event_Handler_Handle_Timeout_Upcall.h"
+#include "../ace_interceptor/aint.h"
+#include "../ace_interceptor/apass.h"
+#include "../ace_interceptor/aenc.h"
 
 #define SUCCESS 0
 #define FAILURE (-1)
@@ -19,12 +22,47 @@
 
 #define MIN_ARGS 2
 
+class sigint_handler: public ACE_Event_Handler {
+public:
+  virtual int handle_signal (int, siginfo_t* = 0, siginfo_t* = 0);
+  virtual int handle_close (ACE_HANDLE, ACE_Reactor_Mask);
+};
+
+  int
+  sigint_handler::handle_signal (int, siginfo_t *, siginfo_t *){
+    
+      ACE_Reactor::instance()->end_reactor_event_loop();
+      ACE_Reactor::instance()->close();
+
+
+    return 0;
+  }
+  int
+  sigint_handler::handle_close (ACE_HANDLE ace_handle, ACE_Reactor_Mask ace_r_mask){
+
+    fprintf(stderr,
+	    "Closing...\n\t"
+	    "TIMEOUT: %d\n\t"
+	    "SIGNAL : %d\n",
+	    !!(ace_r_mask & TIMER_MASK),
+	    !!(ace_r_mask & SIGNAL_MASK));
+    handle_signal(SIGINT, NULL, NULL);
+    return 0;
+  }
+
 
 class my_ev_handler: public ACE_Event_Handler {
   int n_to_send;
   char ** to_send;
+  int_class ** my_int;
+  int nptrs;
 public:
+  void set_interceptor(int_class ** new_int, int n) {
+    my_int = new_int;
+    nptrs = n;
+  }
   my_ev_handler(int send_argc, char ** send_argv) {
+    my_int = NULL;
     this->n_to_send = send_argc;
     this->to_send = send_argv;
   }
@@ -45,7 +83,13 @@ public:
     char send_buf[32];
     for(int i=0; i<this->n_to_send; i++){
       sprintf(send_buf, "%s\n", this->to_send[i]);
-      if(ace_stream.send_n(send_buf, strlen(send_buf)) < 0){
+      int len = strlen(send_buf);
+      for(int l =0; my_int && l<len; l++){
+	for(int n=0; n<nptrs; n++){
+	  (*(my_int[n]))(send_buf[l]);
+	}
+      }
+      if(ace_stream.send_n(send_buf, len) < 0){
 	fprintf(stderr, "Error sending (%d)\n", i);
 	return FAILURE;
       }
@@ -67,9 +111,18 @@ int main(int argc, char * argv[]){
     exit(FAILURE);
   }
 
-
+    //  pass_through * p = new pass_through;
+  int ntest = 50;
+  int_class ** ptrs = (int_class **)calloc(ntest, sizeof(a_enc *));
+  for(int i=0;i<ntest;i++){
+    ptrs[i] = new a_enc(i&0xff);
+  }
   my_ev_handler sender(argc, argv);
+  sender.set_interceptor(ptrs, ntest);
+  sigint_handler h;
 
+  ACE_Reactor::instance()->register_handler(SIGINT,
+					    &h);
 
   ACE_Time_Value t_interval(3, 0);
   ACE_Time_Value t_delay(0, 0);
--- shared_stuff/studios/s12/ace_server/aserver.cpp
+++ shared_stuff/studios/s12/ace_server/aserver.cpp
@@ -11,7 +11,9 @@
 #include "ace/Timer_Heap_T.h"
 #include "ace/Timer_Heap.h"
 #include "ace/Event_Handler_Handle_Timeout_Upcall.h"
-
+#include "../ace_interceptor/aint.h"
+#include "../ace_interceptor/apass.h"
+#include "../ace_interceptor/aenc.h"
 
 #define SUCCESS 0
 #define FAILURE (-1)
@@ -25,13 +27,40 @@
 class my_ev_handler: public ACE_Event_Handler {
   ACE_INET_Addr ace_addr;
   ACE_SOCK_Acceptor ace_accptr;
+  int_class ** my_int;
+  int nptrs;
 public:
+  void set_interceptor(int_class ** new_int, int n) {
+    my_int = new_int;
+    nptrs = n;
+  }
+  bool status;
   my_ev_handler(ACE_SOCK_Acceptor &accptr) : ace_accptr(accptr), ace_addr(PORT_NUM, ACE_LOCALHOST) {
+    my_int = NULL;
+    status = true;
     if(ace_accptr.open(ace_addr, 1) < 0){
       fprintf(stderr, "Error opening acceptor\n");
     }
   }
+  ~my_ev_handler() {
+    status = false;
+  }
 
+
+  int handle_close (ACE_HANDLE ace_handle, ACE_Reactor_Mask ace_r_mask){
+
+    fprintf(stderr,
+	    "Closing...\n\t"
+	    "READ  : %d\n\t"
+	    "Bool  : %d\n",
+	    !!(ace_r_mask & READ_MASK),
+	    this->status);
+    if(this->status == true){
+      delete this;
+    }
+    return 0;
+  }
+  
   int handle_input (ACE_HANDLE h = ACE_INVALID_HANDLE) {
 
     ACE_SOCK_Stream ace_stream;
@@ -47,6 +76,11 @@ public:
     size_t ret = 0;
     char single_read;
     while((ret = ace_stream.recv_n(&single_read, 1)) > 0){
+      if(my_int){
+	for(int n=0; n < nptrs; n++){
+	  (*(my_int[n]))(single_read);
+	}
+      }
       fprintf(stderr, "%c", single_read);
     }
     ace_stream.close();
@@ -58,9 +92,44 @@ public:
   ACE_HANDLE get_handle() const {
     return this->ace_accptr.get_handle();
   }
- 
 };
 
+
+
+
+class sigint_handler: public ACE_Event_Handler {
+  my_ev_handler * ev;
+public:
+  sigint_handler(my_ev_handler * init) {
+    this->ev = init;
+  }
+  virtual int handle_signal (int, siginfo_t* = 0, siginfo_t* = 0);
+  virtual int handle_close (ACE_HANDLE, ACE_Reactor_Mask);
+};
+
+  int
+  sigint_handler::handle_signal (int, siginfo_t *, siginfo_t *){
+    
+      ACE_Reactor::instance()->end_reactor_event_loop();
+      ACE_Reactor::instance()->close();
+
+
+    return 0;
+  }
+  int
+  sigint_handler::handle_close (ACE_HANDLE ace_handle, ACE_Reactor_Mask ace_r_mask){
+
+    fprintf(stderr,
+	    "Closing...\n\t"
+	    "ACCEPT: %d\n\t"
+	    "SIGNAL: %d\n\t"
+	    "Bool  : %d\n",
+	    !!(ace_r_mask & ACCEPT_MASK),
+	    !!(ace_r_mask & SIGNAL_MASK),
+	    this->ev->status);
+    handle_signal(SIGINT, NULL, NULL);
+    return 0;
+  }
 
 int main(int argc, char * argv[]){
   if(argc != NARGS){
@@ -72,14 +141,27 @@ int main(int argc, char * argv[]){
 	  "Arg[0]: %s\n",
 	  argv[0]);
 
+  //  pass_through * p = new pass_through;
+  int ntest = 49;
+  int_class ** ptrs = (int_class **)calloc(ntest, sizeof(a_enc *));
+  for(int i=0;i<ntest;i++){
+    ptrs[i] = new a_enc((i&0xff));
+  }
+
   ACE_SOCK_Acceptor accptr;
-  my_ev_handler sender(accptr);
-  ACE_Reactor::instance()->register_handler(&sender,
+  my_ev_handler * sender = new my_ev_handler(accptr);
+  sender->set_interceptor(ptrs, ntest);
+  sigint_handler h(sender);
+
+  ACE_Reactor::instance()->register_handler(sender,
 					    ACE_Event_Handler::ACCEPT_MASK);
+  ACE_Reactor::instance()->register_handler(SIGINT,
+					    &h);
+
   ACE_Reactor::instance()->run_reactor_event_loop();
   //  while(sender.handle_input() == SUCCESS);
 
-    
+
 
   return FAILURE;
 }
